e0cd7e2a9aa54cb7be4def892fe343cac460faca
chris chen <y_zhengchen@163.com>
2021-06-07 14:30:23 +0800

    对乱码问题的思考

--- src/main/java/com/chris/nio/cases/FileChannelTest.java
+++ src/main/java/com/chris/nio/cases/FileChannelTest.java
@@ -1,6 +1,5 @@
 package com.chris.nio.cases;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -13,14 +12,21 @@ public class FileChannelTest {
         //从文件获取fileChannel
         FileChannel fileChannel = new RandomAccessFile("FileChannelTest.txt", "rw").getChannel();
         //声明一个Byte类型的Buffer
-        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
-        while (fileChannel.read(byteBuffer) > 0) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(20);
+        while (fileChannel.read(byteBuffer) != -1) {
+            // buffer切换为读模式
             byteBuffer.flip();
             if (byteBuffer.hasRemaining()) {
+                //采用字节流的方式，一次次sout，不可避免出现乱码现象
+                //比如中英文，china你好！
+                //在UTF-8编码下，每个中文占3个字节，如果读取的字节数不满3个，则随后的System.out.println()会出现乱码
+                //采用字符流InputStreamReader或者按编码方式申请字节数
                 byte[] bytes = new byte[byteBuffer.remaining()];
                 byteBuffer.get(bytes);
+                //若是直接写到文件没有乱码问题
                 System.out.println(new String(bytes, StandardCharsets.UTF_8));
             }
+            // clear()会将buffer再次切换为写模式
             byteBuffer.clear();
         }
     }
